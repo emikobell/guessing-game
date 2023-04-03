aecabcb69efb4e360bd987dca8ab326ba5c989ea
emikob <emikoelizabethbell@gmail.com>
2023-04-03 13:20:11 -0700

    Added random number generator

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 """A number-guessing game."""
 
+import random
+
 print("Hello! Welcome to the number guessing game!")
 
 name = input("What is your name?")
+
+random_number = random.randrange(1,100)
