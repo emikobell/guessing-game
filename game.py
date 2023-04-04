8970a4c29bd6c2794fd86b936b00e0af4340943a
emikob <emikoelizabethbell@gmail.com>
2023-04-04 09:20:55 -0700

    Added option to play the game again

--- game.py
+++ game.py
@@ -8,28 +8,45 @@ name = input("What is your name? ")
 
 print(f"Welcome, {name}. This program generates a random number between 1-100. Please try to guess what that number is.")
 
-random_number = random.randrange(1,100)
-guess = None
-number_guesses = 0
+def play_game():
 
+    continue_playing = True
 
-while random_number != guess:
+    while continue_playing:
 
-    try:
-        guess = int(input("Your guess? "))
-    except:
-        print("You didn't input a number!! How dare you!!")
-        continue
+        random_number = random.randrange(1,100)
+        guess = None
+        number_guesses = 0
 
-    if guess < 1 or guess > 101:
-        print("How dare you!! Please enter a number between 1-100.") 
-    elif guess != random_number:
-        if guess > random_number:
-            print("Too high! Please try again.")
-            number_guesses += 1
-        elif guess < random_number:
-            print("Too low! Please try again.")
-            number_guesses += 1
+        while random_number != guess:
 
-print(f"Congratulations! You got the correct number! You found the number in {number_guesses} many tries.")
+            try:
+                guess = int(input("Your guess? "))
+            except:
+                print("You didn't input a number!! How dare you!!")
+                continue
+
+            if guess < 1 or guess > 101:
+                print("How dare you!! Please enter a number between 1-100.") 
+            elif guess != random_number:
+                if guess > random_number:
+                    print("Too high! Please try again.")
+                    number_guesses += 1
+                elif guess < random_number:
+                    print("Too low! Please try again.")
+                    number_guesses += 1
+
+        print(f"Congratulations! You got the correct number! You found the number in {number_guesses} many tries.")
+
+        play_again = input("Would you like to play again? Type y for yes, and n for no. ")
+
+        if play_again == "y":
+            continue
+        elif play_again == "n":
+            continue_playing = False
+        else:
+            print("Invalid answer. Game will stop.")
+            continue_playing = False
+
+play_game()
 
